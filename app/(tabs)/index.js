de18da14672fc3a262e51e314596a7bb079746bc
Bruno Alpachino <brunolalpachino@gmail.com>
2025-06-30 21:40:12 +0300

    INIT: project initialisation

---
+++ app/(tabs)/index.js
@@ -0,0 +1,33 @@
+import { StyleSheet } from "react-native";
+
+import { Canvas } from "@react-three/fiber";
+
+export default function HomeScreen() {
+  return (
+    <Canvas>
+      <mesh>
+        <sphereGeometry />
+        <meshStandardMaterial color={"orange"} />
+      </mesh>
+    </Canvas>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: "absolute",
+  },
+});
